bf43577487f5cfc3a04cb69b8014b33995a86ef7
Edoardo Soloperto <soloperto@nedgenerators.com>
2021-11-29 13:06:36 +0100

    add sqlite3 database storage add prisma ORM library

---
+++ prisma/migrations/20211129112059_init/migration.sql
@@ -0,0 +1,7 @@
+-- CreateTable
+CREATE TABLE "Client" (
+    "id" INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
+    "ip" TEXT NOT NULL,
+    "mac" TEXT NOT NULL,
+    "hostname" TEXT
+);
--- routes/index.js
+++ routes/index.js
@@ -1,12 +1,54 @@
 var express = require('express');
 var router = express.Router();
 const ClientManager = require("./clientManager");
+const Client = require('../models/client.js');
+
+const { PrismaClient } = require('@prisma/client')
+const prisma = new PrismaClient()
 
 var clientManager = new ClientManager();
 
+async function getClient(id) {
+  var client = await prisma.client.findUnique({
+    where: {
+      id: parseInt(id)
+    }
+  });
+
+  return new Client(client);
+}
+
 /* GET home page. */
-router.get('/', function (req, res, next) {
-  res.render('index');
+router.get('/', async function (req, res, next) {
+  const clients = await prisma.client.findMany()
+  res.render('index', { clients: clients });
+});
+
+/*
+* open page in remote browser
+*/
+router.post('/openUrl', async function (req, res, next) {
+  var client = await getClient(req.body.id);
+  var response = client.openUrl(req.body.url, client.ip_address)
+  res.json(response)
+})
+
+/*
+* take screenshot of remote client screen
+*/
+router.get('/getScreenshot/:id', async function (req, res, next) {
+  var client = await getClient(req.params.id);
+  var result = await client.getScreenshot();
+  res.json(result);
+});
+
+/*
+* close all browser windows to remote client
+*/
+router.get('/closeRemoteBrowser/:id', async function (req, res, next) {
+  var client = await getClient(req.params.id);
+  const response = await client.closeAllBrowser();
+  res.json(response)
 });
 
 
@@ -14,26 +56,6 @@ router.get('/ipList/:test', async function (req, res, next) {
   var test = JSON.parse(req.params.test)
   var list = await clientManager.getClientList(test);
   res.json(list);
-});
-
-router.post('/remoteCommand', async function (req, res, next) {
-  var response = await clientManager.remoteCommand(req.body);
-  console.log('remoteCommand response', response)
-  res.json(response)
-})
-
-router.get('/getScreenshot/:id', async function (req, res, next) {
-  var id = req.params.id;
-  console.log("id", id)
-  var result = await clientManager.getScreenshot(id);
-  res.json(result);
-});
-
-router.get('/closeRemoteBrowser/:id', async function (req, res, next) {
-  var id = req.params.id;
-  console.log("closeAll id", id)
-  var result = await clientManager.closeAllRemoteBrowser(id);
-  res.json(result);
 });
 
 module.exports = router;
